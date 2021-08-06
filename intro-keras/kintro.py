96cd8c993b061bf8e509027f2f522c703d0bdb05
Dmitri Soshnikov <dmitri@soshnikov.com>
2021-08-06 14:28:12 +0300

    Add kintro.py module

---
+++ intro-keras/kintro.py
@@ -0,0 +1,80 @@
+# Additional functions for Microsoft Learn Module
+# Introduction to Keras
+
+def read_images(path: str, image_size: int, num_items: int) -> np.ndarray:
+  f = gzip.open(path,'r')
+  f.read(16)
+  buf = f.read(image_size * image_size * num_items)
+  data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+  data = data.reshape(num_items, image_size, image_size)
+  return data
+
+def read_labels(path: str, num_items: int) -> np.ndarray:
+  f = gzip.open(path,'r')
+  f.read(8)
+  buf = f.read(num_items)
+  data = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
+  data = data.reshape(num_items)
+  return data
+
+def read_images(path: str, image_size: int, num_items: int) -> np.ndarray:
+  f = gzip.open(path,'r')
+  buf = f.read(image_size * image_size * num_items)
+  data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+  data = data.reshape(num_items, image_size, image_size)
+  return data
+
+def read_labels(path: str, num_items: int) -> np.ndarray:
+  f = gzip.open(path,'r')
+  f.read(8)
+  buf = f.read(num_items)
+  data = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
+  data = data.reshape(num_items)
+  return data
+
+def get_data(batch_size: int) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
+  image_size = 28
+  num_train = 60000
+  num_test = 10000
+
+  training_images = read_images('data/FashionMNIST/raw/train-images-idx3-ubyte.gz', image_size, num_train)
+  test_images = read_images('data/FashionMNIST/raw/t10k-images-idx3-ubyte.gz', image_size, num_test)
+  training_labels = read_labels('data/FashionMNIST/raw/train-labels-idx1-ubyte.gz', num_train)
+  test_labels = read_labels('data/FashionMNIST/raw/t10k-labels-idx1-ubyte.gz', num_test)
+
+  # (training_images, training_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
+
+  train_dataset = tf.data.Dataset.from_tensor_slices((training_images, training_labels))
+  test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
+
+  train_dataset = train_dataset.map(lambda image, label: (float(image) / 255.0, label))
+  test_dataset = test_dataset.map(lambda image, label: (float(image) / 255.0, label))
+
+  train_dataset = train_dataset.batch(batch_size).shuffle(500)
+  test_dataset = test_dataset.batch(batch_size).shuffle(500)
+
+  return (train_dataset, test_dataset)
+
+
+def get_model() -> tf.keras.Sequential:
+  model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(10)
+  ])
+  return model
+
+labels_map = {
+  0: 'T-Shirt',
+  1: 'Trouser',
+  2: 'Pullover',
+  3: 'Dress',
+  4: 'Coat',
+  5: 'Sandal',
+  6: 'Shirt',
+  7: 'Sneaker',
+  8: 'Bag',
+  9: 'Ankle Boot',
+}
+
