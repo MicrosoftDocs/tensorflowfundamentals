ccc0fa119dce1122529c2400d395542fbd7cf3b6
Dmitri Soshnikov <dmitri@soshnikov.com>
2021-08-28 00:42:38 +0300

    Update cv, nlp and audio with latest code

--- computer-vision-tf/tfcv.py
+++ computer-vision-tf/tfcv.py
@@ -1,4 +1,4 @@
-# Tensorflow Computer Vision Helper
+# TensorFlow Computer Vision Helper
 
 import tensorflow as tf
 from tensorflow import keras
@@ -8,7 +8,7 @@ from PIL import Image
 import glob
 import os
 
-def plot_convolution_tf(data,t,title=''):
+def plot_convolution(data,t,title=''):
     fig, ax = plt.subplots(2,len(data)+1,figsize=(8,3))
     fig.suptitle(title,fontsize=16)
     tt = np.expand_dims(np.expand_dims(t,2),2)
@@ -24,66 +24,6 @@ def plot_convolution_tf(data,t,title=''):
     ax[1,-1].axis('off')
     #plt.tight_layout()
     plt.show()
-
-# Taken from https://medium.com/analytics-vidhya/2d-convolution-using-python-numpy-43442ff5f381
-def convolve2D(image, kernel, padding=0, strides=1):
-    # Cross Correlation
-    kernel = np.flipud(np.fliplr(kernel))
-
-    # Gather Shapes of Kernel + Image + Padding
-    xKernShape = kernel.shape[0]
-    yKernShape = kernel.shape[1]
-    xImgShape = image.shape[0]
-    yImgShape = image.shape[1]
-
-    # Shape of Output Convolution
-    xOutput = int(((xImgShape - xKernShape + 2 * padding) / strides) + 1)
-    yOutput = int(((yImgShape - yKernShape + 2 * padding) / strides) + 1)
-    output = np.zeros((xOutput, yOutput))
-
-    # Apply Equal Padding to All Sides
-    if padding != 0:
-        imagePadded = np.zeros((image.shape[0] + padding*2, image.shape[1] + padding*2))
-        imagePadded[int(padding):int(-1 * padding), int(padding):int(-1 * padding)] = image
-        print(imagePadded)
-    else:
-        imagePadded = image
-
-    # Iterate through image
-    for y in range(image.shape[1]):
-        # Exit Convolution
-        if y > image.shape[1] - yKernShape:
-            break
-        # Only Convolve if y has gone down by the specified Strides
-        if y % strides == 0:
-            for x in range(image.shape[0]):
-                # Go to next row once kernel is out of bounds
-                if x > image.shape[0] - xKernShape:
-                    break
-                try:
-                    # Only Convolve if x has moved by the specified Strides
-                    if x % strides == 0:
-                        output[x, y] = (kernel * imagePadded[x: x + xKernShape, y: y + yKernShape]).sum()
-                except:
-                    break
-
-    return output
-
-def plot_convolution(data,t,title=''):
-    fig, ax = plt.subplots(2,len(data)+1,figsize=(8,3))
-    fig.suptitle(title,fontsize=16)
-    for i,im in enumerate(data):
-        ax[0][i].imshow(im)
-        cim = convolve2D(im,t)
-        ax[1][i].imshow(cim)
-        ax[0][i].axis('off')
-        ax[1][i].axis('off')
-    ax[0,-1].imshow(t)
-    ax[0,-1].axis('off')
-    ax[1,-1].axis('off')
-    #plt.tight_layout()
-    plt.show()
-
 
 def plot_results(hist):
     fig,ax = plt.subplots(1,2,figsize=(15,3))
